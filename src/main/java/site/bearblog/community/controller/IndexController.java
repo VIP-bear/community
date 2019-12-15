fe3ac8cf62d949ab3273da263f0ee28b27b2a665
VIP-bear <1907464379@qq.com>
2019-12-15 13:27:04 +0800

    paging function

--- src/main/java/site/bearblog/community/controller/IndexController.java
+++ src/main/java/site/bearblog/community/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import site.bearblog.community.dto.PaginationDTO;
 import site.bearblog.community.dto.QuestionDTO;
 import site.bearblog.community.mapper.QuestionMapper;
 import site.bearblog.community.mapper.UserMapper;
@@ -27,7 +29,9 @@ public class IndexController {
     // 进入首页，拿到cookie中的token的value，去数据库中查询
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -41,8 +45,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/site/bearblog/community/service/QuestionService.java
+++ src/main/java/site/bearblog/community/service/QuestionService.java
@@ -3,6 +3,7 @@ package site.bearblog.community.service;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import site.bearblog.community.dto.PaginationDTO;
 import site.bearblog.community.dto.QuestionDTO;
 import site.bearblog.community.mapper.QuestionMapper;
 import site.bearblog.community.mapper.UserMapper;
@@ -21,8 +22,20 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list(){
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size){
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+        // 容错处理
+        if (page < 1){
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
         for (Question question : questions){
             User user = userMapper.finById(question.getCreator());
@@ -31,6 +44,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
